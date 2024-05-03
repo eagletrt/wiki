af99be61e9c19f74321aafd1cc5a5597384aaea9
Filippo <pippo.faccini@gmail.com>
2024-05-03 16:51:19 +0200

    feat: update ignored github repos

--- ignored.py
+++ ignored.py
@@ -1,2 +1,2 @@
 # repositories inside this array will be ignored when generating docs
-IGNORED = ["wiki", "c-template"]
+IGNORED = ["wiki", "c-template", "hv-bms-mainboard-hw", "telemetry-app", "telemetry", "hv-bms-cellboard-hw", "eagle-kicad-library"]
