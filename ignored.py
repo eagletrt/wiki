b485657cb8e2a034dffd5b1581b770bcf3ced349
Filippo <pippo.faccini@gmail.com>
2024-05-13 23:20:39 +0200

    feat: remove telemetry repos from ignored.py

--- ignored.py
+++ ignored.py
@@ -1,2 +1,2 @@
 # repositories inside this array will be ignored when generating docs
-IGNORED = ["wiki", "c-template", "hv-bms-mainboard-hw", "telemetry-app", "telemetry", "hv-bms-cellboard-hw", "eagle-kicad-library"]
+IGNORED = ["wiki", "c-template", "hv-bms-mainboard-hw", "hv-bms-cellboard-hw", "eagle-kicad-library"]
